84eb72c44ac72ec2c6c9a844a807bd2b222da9bf
bcebel <bcebel@gmail.com>
2023-05-12 14:51:59 -0400

    third model

---
+++ models/Reaction.js
@@ -0,0 +1,23 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
--- models/Thoughts.js
+++ models/Thoughts.js
@@ -3,7 +3,11 @@ const { Schema, model, SchemaTypes } = mongoose;
 
 const ThoughtSchema = new Schema({
   thoughtText: { type: String, required: true, minlength: 1, maxlength: 280 },
-  createdAt: { type: Date, default: Date.now },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal),
+  },
   username: { type: String, required: true },
   reactions: [ReactionSchema],
 });
